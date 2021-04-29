4d7a683c618c5a04747f2b11858ed34721731cb3
Chuviliaeva Irina <chuviliaeva.ip@phystech.edu>
2021-04-29 06:43:14 +0300

    fix all memory manager

--- allocator/mm.h
+++ allocator/mm.h
@@ -27,22 +27,29 @@ namespace lab618
         std::cout << "size = " << size << std::endl;
         std::cout << "as int:" << std::endl;
         for(int i = 0; i < size; ++i) {
-          int q = *(reinterpret_cast<int*>(pdata + i * sizeof(T)));
+          int q = *(reinterpret_cast<int*>(reinterpret_cast<char*>(pdata) + i * sizeof(T)));
           std::cout << q << " ";
         }
+        std::cout << std::endl;
         bool is_t[size];
-        memset(is_t, true, size);
         int not_t = firstFreeIndex;
+        memset(is_t, true, size);
         while(not_t != -1) {
           is_t[not_t] = false;
-          not_t = *(reinterpret_cast<int*>(pdata + not_t * sizeof(T)));
+          not_t = *(reinterpret_cast<int*>(reinterpret_cast<char*>(pdata) + not_t * sizeof(T)));
+        }
+        std::cout << std::endl << "is_t:" << std::endl;
+        for(int i = 0; i < size; ++i) {
+          std::cout << is_t[i] << " ";
         }
         std::cout << std::endl;
         std::cout << "as T:" << std::endl;
         for(int i = 0; i < size; ++i) {
           if(is_t[i]) {
-            T q = *(reinterpret_cast<T*>(pdata + i * sizeof(T)));
+            T q = *(reinterpret_cast<T*>(reinterpret_cast<char*>(pdata) + i * sizeof(T)));
             q.toString();
+          } else {
+            std::cout << "no T" << std::endl;
           }
         }
         std::cout << std::endl;
@@ -57,8 +64,12 @@ namespace lab618
 
   public:
     CMemoryManager(int _default_block_size, bool isDeleteElementsOnDestruct = false):
-      m_blkSize(_default_block_size), m_isDeleteElementsOnDestruct(isDeleteElementsOnDestruct), m_pBlocks(nullptr),
-      m_pCurrentBlk(nullptr){}
+      m_blkSize(_default_block_size), m_isDeleteElementsOnDestruct(isDeleteElementsOnDestruct), m_pBlocks(0),
+      m_pCurrentBlk(0){
+      if(_default_block_size <= 0) {
+        throw("Can't use less than 1 element in block");
+      }
+    }
 
     virtual ~CMemoryManager()
     {
@@ -68,15 +79,16 @@ namespace lab618
     // Получить адрес нового элемента из менеджера
     T* newObject()
     {
-      if(m_pBlocks == nullptr) {
-        m_pBlocks = newBlock();
-        m_pCurrentBlk = m_pBlocks;
+      if(m_pBlocks == 0) { // менаджер ещё ничего не выделял, создаём новый блок
+        m_pCurrentBlk = m_pBlocks = newBlock(); // он совсем пустой и в указателе на следующий будет 0
       }
+      // в менаджере что-то есть, значит есть и текущий блок
       if(m_pCurrentBlk->firstFreeIndex == -1) {
+        // в текущем блоке кончилось место, ищем первый пустой и делаем его текущим
         bool find = false;
         block* cur = m_pBlocks;
-        block* last = nullptr;
-        while(!find && cur != nullptr){
+        block* last = 0; // здесь должен оказаться последний выделенный блок, если не найдём свободного места
+        while(!find && cur != 0) { //m_pBlocks != 0 знчит хоть раз зайдём и не можем получить last == 0
           if(cur->firstFreeIndex != -1) {
             find = true;
             m_pCurrentBlk = cur;
@@ -85,65 +97,85 @@ namespace lab618
             cur = cur->pnext;
           }
         }
-        if (!find) {
+        if (!find) { // Нет места во всех выделенных блоках, выделяем новый
           m_pCurrentBlk = newBlock();
-          if(last != nullptr) {
-            last->pnext = m_pCurrentBlk;
-          }
+          last->pnext = m_pCurrentBlk;
         }
       }
-      int free_index = m_pCurrentBlk->firstFreeIndex;
-      int next_free = *(reinterpret_cast<int*>(m_pCurrentBlk->pdata + free_index * sizeof(T)));
+      // После всего сделанного m_pCurrent точно содержит указатель на блок, где есть свободное место
+      int free_index = m_pCurrentBlk->firstFreeIndex; // точно >= 0
+      int next_free = *(reinterpret_cast<int*>(reinterpret_cast<char*>(m_pCurrentBlk->pdata) + free_index * sizeof(T)));
       m_pCurrentBlk->usedCount++;
       m_pCurrentBlk->firstFreeIndex = next_free;
-      memset(reinterpret_cast<char*>(m_pCurrentBlk->pdata + free_index * sizeof(T)), 0, sizeof(T));
-      ::new(reinterpret_cast<char*>(m_pCurrentBlk->pdata + free_index * sizeof(T))) T;
-      return reinterpret_cast<T*>(m_pCurrentBlk->pdata + free_index * sizeof(T));
+      memset(reinterpret_cast<void*>(reinterpret_cast<char*>(m_pCurrentBlk->pdata) + free_index * sizeof(T)),
+             0, sizeof(T));
+      ::new(reinterpret_cast<void*>(reinterpret_cast<char*>(m_pCurrentBlk->pdata) + free_index * sizeof(T))) T;
+      return reinterpret_cast<T*>(reinterpret_cast<char*>(m_pCurrentBlk->pdata) + free_index * sizeof(T));
     }
 
     // Освободить элемент в менеджере
 
     bool deleteObject(T* p)
     {
-      /* перебрать блок и найти по-умному через сравнения*/
+      // перебираем блок, ищем блок с нашим элементом
       block* block_with_it = m_pBlocks;
       int it_index;
       bool find = false;
-      while(!find && block_with_it != nullptr){
-        if(p <= reinterpret_cast<T*>(block_with_it->pdata) &&
-            p >= reinterpret_cast<T*>(block_with_it->pdata + (m_blkSize - 1) * sizeof(T))) {
+      while(!find && block_with_it != 0){
+        if(p >= reinterpret_cast<T*>(block_with_it->pdata) &&
+            p <= reinterpret_cast<T*>(reinterpret_cast<char*>(block_with_it->pdata) + (m_blkSize - 1) * sizeof(T))) {
           for(int i = 0; (i < m_blkSize && !find); ++i) {
-            T* t_iter = reinterpret_cast<T*>(block_with_it->pdata + i * sizeof(T));
+            T* t_iter = reinterpret_cast<T*>(reinterpret_cast<char*>(block_with_it->pdata) + i * sizeof(T));
             if(t_iter == p){
               it_index = i;
               find = true;
             }
           }
         }
-
-        block_with_it = block_with_it->pnext;
+        if(!find){
+          block_with_it = block_with_it->pnext;
+        }
       }
-      if(block_with_it == nullptr) {
+      if(block_with_it == 0) { // перебрали все блоки и не нашли, значит такого элемента нет
         return false;
       }
-      int last_free_index = block_with_it->firstFreeIndex;
-      block_with_it->firstFreeIndex = it_index;
+      // Теперь в block_with_it лежит указатель на блок с элементом, но он может быть уже удалён
+      bool is_t[m_blkSize];
+      memset(is_t, true, m_blkSize);
+      int free_index = block_with_it->firstFreeIndex;
+      while(free_index != -1) {
+        is_t[free_index] = false;
+        free_index = *(reinterpret_cast<int*>(reinterpret_cast<char*>(block_with_it->pdata) + free_index * sizeof(T)));
+      }
+      if(!is_t[it_index]) {
+        return false;
+      }
+      // удаляем сам элемент
       p->~T();
-      memset(reinterpret_cast<char*>(p), 0, sizeof(T));
-      block_with_it->pdata[it_index] = last_free_index;
+      memset(reinterpret_cast<void*>(reinterpret_cast<char*>(block_with_it->pdata) + it_index * sizeof(T)),
+             0, sizeof(T));
       block_with_it->usedCount--;
-      return true;
+
+      // Переставляем индексы: индекс новой свободной ячейки становится в начало последовательности
+      int was_free_index = block_with_it->firstFreeIndex;
+      block_with_it->firstFreeIndex = it_index;
+      int* place_for_old_index = reinterpret_cast<int*>(reinterpret_cast<char*>(block_with_it->pdata) +
+                                                    it_index * sizeof(T));
+      *place_for_old_index = was_free_index;
+      return true; // вернули, что удалили успешно
     }
 
     // Очистка данных, зависит от m_isDeleteElementOnDestruct
     void clear()
     {
       block* cur = m_pBlocks;
-      while(cur != nullptr) {
+      while(cur != 0) {
         block* next = cur->pnext;
         deleteBlock(cur); //вот эта функция зависит от m_isDeleteElementOnDestruct
         cur = next;
       }
+      m_pBlocks = 0;
+      m_pCurrentBlk = 0;
     }
 
     void ToString() {
@@ -154,7 +186,7 @@ namespace lab618
       std::cout << "=======blocks=======" << std::endl;
 
       block* cur_blk = m_pBlocks;
-      while(cur_blk != nullptr) {
+      while(cur_blk != 0) {
         cur_blk->ToString(m_blkSize);
         cur_blk = cur_blk->pnext;
       }
@@ -166,16 +198,15 @@ namespace lab618
     {
       block* new_block = new block;
       new_block->pdata = reinterpret_cast<T*>(new char[sizeof(T) * m_blkSize]);
-      new_block->pnext = nullptr;
       for(int i = 0; i < m_blkSize; ++i){
-        int* place_int = reinterpret_cast<int*>(new_block->pdata + i * sizeof(T));
+        int* place_int = reinterpret_cast<int*>(reinterpret_cast<char*>(new_block->pdata) + i * sizeof(T));
         *place_int = i + 1;
-        new_block->ToString();
       }
-      int* last_place = reinterpret_cast<int*>(new_block->pdata + (m_blkSize - 1) * sizeof(T));
+      int* last_place = reinterpret_cast<int*>(reinterpret_cast<char*>(new_block->pdata) + (m_blkSize - 1) * sizeof(T));
       *last_place = -1;
       new_block->firstFreeIndex = 0;
       new_block->usedCount = 0;
+      new_block->pnext = 0;
       return new_block;
     }
 
@@ -184,18 +215,18 @@ namespace lab618
     void deleteBlock(block *p)
     {
       if(!m_isDeleteElementsOnDestruct && p->usedCount != 0) {
-        throw CException();
+        throw("You have to delete all elements!");
       }
       bool is_t[m_blkSize];
       memset(is_t, true, m_blkSize);
       int not_t = p->firstFreeIndex;
       while(not_t != -1) {
         is_t[not_t] = false;
-        not_t = *(reinterpret_cast<int*>(p->pdata + not_t * sizeof(T)));
+        not_t = *(reinterpret_cast<int*>(reinterpret_cast<char*>(p->pdata) + not_t * sizeof(T)));
       }
       for(int i = 0; i < m_blkSize; ++i) {
         if(is_t[i]){
-          reinterpret_cast<T*>(p->pdata + i * sizeof(T))->~T();
+          reinterpret_cast<T*>(reinterpret_cast<char*>(p->pdata) + i * sizeof(T))->~T();
         }
       }
       delete[] reinterpret_cast<char*>(p->pdata);
--- allocator/test_allocator.cpp
+++ allocator/test_allocator.cpp
@@ -42,21 +42,160 @@ static void generate(TestStruct *pts)
   pts->value2 = makeRandomString();
 }
 
-void TestAllocatorFunction()
+void TestAllocatorNewFunction(int block_size, int num_el)
 {
-  int block_size = 42;
-  int num_el = 47;
   TestManager manager = TestManager (block_size, true);
-  /*for(int i = 0; i < num_el; ++i) {
+  for(int i = 0; i < num_el; ++i) {
     TestStruct ts;
     generate(&ts);
     TestStruct* iterator = manager.newObject();
     *iterator = ts;
-  }*/
-  manager.ToString();
+  }
+  manager.clear();
+}
+
+void TestAllocatorDeleteFunction(int block_size, int num_el) {
+  TestManager good_manager = TestManager(block_size, true);
+  TestManager bad_manager = TestManager(block_size, false);
+  TestStruct* pointers[num_el];
+  for(int i = 0; i < num_el; ++i) {
+    TestStruct ts;
+    generate(&ts);
+    TestStruct* iterator = good_manager.newObject();
+    *iterator = ts;
+    pointers[i] = iterator;
+  }
+
+  for(int i = 0; i < num_el; ++i) {
+    assert(good_manager.deleteObject(pointers[i]) == true);
+  }
+  good_manager.clear();
+
+  for(int i = 0; i < num_el; ++i) {
+    TestStruct ts;
+    generate(&ts);
+    TestStruct* iterator = bad_manager.newObject();
+    *iterator = ts;
+    pointers[i] = iterator;
+  }
+
+  for(int i = 0; i < num_el; ++i) {
+    bad_manager.deleteObject(pointers[i]);
+  }
+  bad_manager.clear();
+
+  for(int i = 0; i < num_el; ++i) {
+    TestStruct ts;
+    generate(&ts);
+    TestStruct* iterator = good_manager.newObject();
+    *iterator = ts;
+    pointers[i] = iterator;
+  }
+
+  for(int i = 0; i < num_el; ++i) {
+    if(i % 3 == 0){
+      good_manager.deleteObject(pointers[i]);
+    }
+  }
+  good_manager.clear();
+
+  for(int i = 0; i < num_el; ++i) {
+    TestStruct ts;
+    generate(&ts);
+    TestStruct* iterator = bad_manager.newObject();
+    *iterator = ts;
+    pointers[i] = iterator;
+  }
+
+  for(int i = 0; i < num_el; ++i) {
+    if(i % 5 == 0){
+      bad_manager.deleteObject(pointers[i]);
+    }
+  }
+  for(int i = 0; i < num_el; ++i) {
+    if(i % 5 == 0) {
+      assert(bad_manager.deleteObject(pointers[i]) == false);
+    } else {
+      assert(bad_manager.deleteObject(pointers[i]) == true);
+    }
+  }
+  bad_manager.clear();
+
+
+  for(int i = 0; i < num_el; ++i) {
+    TestStruct ts;
+    generate(&ts);
+    TestStruct* iterator = good_manager.newObject();
+    *iterator = ts;
+    pointers[i] = iterator;
+  }
+
+  for(int i = num_el - 1; i >= 0; --i) {
+    assert(good_manager.deleteObject(pointers[i]) == true);
+  }
+  good_manager.clear();
+
+  for(int i = 0; i < num_el; ++i) {
+    TestStruct ts;
+    generate(&ts);
+    TestStruct* iterator = bad_manager.newObject();
+    *iterator = ts;
+    pointers[i] = iterator;
+  }
+
+  for(int i = num_el - 1; i >= 0; --i) {
+    assert(bad_manager.deleteObject(pointers[i]) == true);
+  }
+  bad_manager.clear();
+
+}
+
+void TestAllocatorRandomDeleteFunction(int block_size, int num_el, int num_deletions){
+  TestManager good_manager = TestManager(block_size, true);
+  TestManager bad_manager = TestManager(block_size, false);
+  TestStruct* pointers[num_el];
+  for(int i = 0; i < num_el; ++i) {
+    TestStruct ts;
+    generate(&ts);
+    TestStruct* iterator = good_manager.newObject();
+    *iterator = ts;
+    pointers[i] = iterator;
+  }
+  for(int i = 0; i < num_deletions; ++i) {
+    good_manager.deleteObject(pointers[rand() % num_el]);
+  }
+  good_manager.clear();
+
+  for(int i = 0; i < num_el; ++i) {
+    TestStruct ts;
+    generate(&ts);
+    TestStruct* iterator = bad_manager.newObject();
+    *iterator = ts;
+    pointers[i] = iterator;
+  }
+  for(int i = 0; i < num_deletions; ++i) {
+    bad_manager.deleteObject(pointers[rand() % num_el]);
+  }
+  for(int i = 0; i < num_el; ++i) {
+    bad_manager.deleteObject(pointers[i]);
+  }
+  bad_manager.clear();
 }
 
 int main() {
-  TestAllocatorFunction();
+  TestAllocatorNewFunction(1, 10);
+  TestAllocatorNewFunction(2, 55);
+  TestAllocatorNewFunction(10, 99);
+  TestAllocatorNewFunction(100, 1000);
+  TestAllocatorNewFunction(10, 0);
+
+  TestAllocatorDeleteFunction(1, 10);
+  TestAllocatorDeleteFunction(2, 55);
+  TestAllocatorDeleteFunction(10, 99);
+  TestAllocatorDeleteFunction(100, 1000);
+  TestAllocatorDeleteFunction(10, 0);
+
+  TestAllocatorRandomDeleteFunction(10, 1000, 200);
+  TestAllocatorRandomDeleteFunction(13, 1000, 1000);
   return 0;
 }
